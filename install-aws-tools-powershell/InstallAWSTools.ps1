c24b72f3ea03992934a2e6357d9b357e8ad5c599
Sourav Kundu <skundu.dev@gmail.com>
2022-09-17 12:06:45 -0500

    updated script to be scalable

--- install-aws-tools-powershell/InstallAWSTools.ps1
+++ install-aws-tools-powershell/InstallAWSTools.ps1
@@ -1,40 +1,28 @@
-# This script checks and installs aws.tools.installer and aws.tools.common modules
+# This script checks and installs aws.tools.installer and aws.tools.common modules 
+# and any other module specfied in the $RequiredModules list
 
-# Create a list of all modules installed and then identify if the required modules are already installed.
+# Create a list of all modules already installed and 
+# then identify if the required modules are already installed. Read Idempotent.
 $ListofModulesInstalled = (Get-InstalledModule).Name
+# These are the modules being installed:
+# https://www.powershellgallery.com/packages/AWS.Tools.Installer
+# https://www.powershellgallery.com/packages/AWS.Tools.Common
+# Populate below list with modules based on your requirement
+# More modules could be found at: https://www.powershellgallery.com/packages?q=AWS.Tools
 $RequiredModules = @(
                         "AWS.Tools.Installer",
                         "AWS.Tools.Common"
                     )
+
 foreach ($module in $RequiredModules)
 {
     Write-Host "Checking if $module is installed on this instance."
     if ($ListofModulesInstalled -contains "$module")
     { 
         Write-Host "$module module exists."
-    }else { 
+    } else { 
         Write-Host "$module module does not exist and needs to be installed."
         Install-Module -Name $module -Force
         Write-Host "$module was installed successfully."
     }
 }
-# # https://www.powershellgallery.com/packages/AWS.Tools.Installer/1.0.2.4
-# Write-Host "Checking if AWS.Tools.Installer is installed on this instance."
-#     if ($ListofModulesInstalled -contains "AWS.Tools.Installer")
-#     { 
-#         Write-Host "AWS.Tools.Installer module exists."
-#     }else { 
-#         Write-Host "AWS.Tools.Installer module does not exist and needs to be installed."
-#         Install-Module -Name AWS.Tools.Installer -Force
-#         Write-Host "AWS.Tools.Installer was installed successfully."
-#     }
-# # https://www.powershellgallery.com/packages/AWS.Tools.Common/4.1.50
-# Write-Host "Checking if AWS.Tools.Common is installed on this instance."
-#     if ($ListofModulesInstalled -contains "AWS.Tools.Common")
-#     { 
-#         Write-Host "AWS.Tools.Common module exists."
-#     }else { 
-#         Write-Host "AWS.Tools.Common module does not exist and needs to be installed."
-#         Install-Module -Name AWS.Tools.Common -Force
-#         Write-Host "AWS.Tools.Common was installed successfully."
-#     }
